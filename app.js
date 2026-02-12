44e45d3ad8f8094e025daea3c90ab8c1aaf1646f
sweetakumari255 <aksweta91422@gmail.com>
2026-02-12 13:36:09 +0530

    updated

---
+++ app.js
@@ -0,0 +1,46 @@
+
+
+
+
+
+
+
+
+
+//------------------------------------------------------------------
+// let a=true;
+// setTimeout(()=>{
+//     let count=0;
+//     while(a){
+//         count++;
+//         console.log(count);
+//     }
+// },0);
+// a=!a;
+
+
+
+//------------------------------------------------------------
+
+// console.log("one");
+// setTimeout(()=>{
+//     console.log("one.one");
+// },2000)
+
+// console.log("two");
+// setTimeout(()=>{
+//     console.log("two.one");
+// },0)
+
+// console.log("three");
+// setTimeout(()=>{
+//     console.log("three.one");
+// },1000)
+
+//------------------------------------------------
+
+// console.log("welcme");
+// setTimeout(()=>{
+//     console.log("hello");
+// },0);
+// console.log("user");
